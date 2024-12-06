68d891fd677ade65a74193454b8817efc713d39e
hspayde <spaydeht@hendrix.edu>
2024-12-06 02:40:24 -0600

    Added Events to calendar

--- calorietrackerplusplus/lib/pages/calendar_page.dart
+++ calorietrackerplusplus/lib/pages/calendar_page.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:calorietrackerplusplus/log_calories_dialog.dart';
 import 'package:calorietrackerplusplus/set_goal_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart'
@@ -11,7 +13,36 @@ import 'package:calorietrackerplusplus/src/authentication.dart';
 import 'package:calorietrackerplusplus/src/widgets.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-import '../friend_card.dart';
+class Event {
+  final String title;
+
+  const Event(this.title);
+
+  @override
+  String toString() => title;
+}
+
+final kEvents = LinkedHashMap<DateTime, List<Event>>( // From https://github.com/aleksanderwozniak/table_calendar/blob/master/example/lib/utils.dart
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(_kEventSource);
+
+final _kEventSource = { for (var item in List.generate(50, (index) => index)) DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5) : List.generate(
+        item % 4 + 1, (index) => Event('Event $item | ${index + 1}')) }
+  ..addAll({
+    kToday: [
+      const Event('Today\'s Event 1'),
+      const Event('Today\'s Event 2'),
+    ],
+  });
+
+  int getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
+
+  final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
+  final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
+  final kToday = DateTime.now();
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -33,6 +64,10 @@ class CalendarPageState extends State<CalendarPage> {
     super.initState();
   }
 
+  List<Event> _getEventsForDay(DateTime day) {
+              return kEvents[day] ?? [];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +79,7 @@ class CalendarPageState extends State<CalendarPage> {
       ),
       body: Column(
         children: [
-          TableCalendar(
+          TableCalendar(  // A lot of this code is from the PubDev documentation  https://pub.dev/packages/table_calendar
             focusedDay: DateTime.now(),
             firstDay: DateTime(2024, 1, 1),
             lastDay: DateTime(2025, 12, 30),
@@ -63,6 +98,12 @@ class CalendarPageState extends State<CalendarPage> {
               setState(() {
                 _calendarFormat = format;
               });
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+            eventLoader: (day) {
+              return _getEventsForDay(day);
             },
           ),
         ],
